acd85892ee5d7997500119d87a6a2b6326eee87e
TAKAM TAYO Aubin Patrick <atakamtay@devoteam.com>
2023-12-13 10:48:31 +0100

    terraform code

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,29 +7,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">=3.7.0"
     }
-    azapi = {
-      source = "azure/azapi"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name  = "rg-ito-maz-prd-neu-management"
-    storage_account_name = "stitoaztfstate"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = false
-      recover_soft_deleted_key_vaults = true
-    }
-  }
-  skip_provider_registration = true
-  use_msi                    = true
-}
-
-provider "azapi" {
+  client_id = ""
+  client_secret = ""
+  subscription_id = ""
+  tenant_id = ""
+  features {}
 }
