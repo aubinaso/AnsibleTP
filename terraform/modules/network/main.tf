236391bf301d6cfc3f219423873cfba7c1f4adc0
Aubin Patrick TAKAM TAYO <62132512+aubinaso@users.noreply.github.com>
2025-06-05 13:17:44 +0200

    Update main.tf

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -11,3 +11,26 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnetautomation.name
   address_prefixes     = var.subnet_prefix
 }
+
+resource "azurerm_network_security_group" "nsg_for_ssh" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 330
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg_for_ssh.id
+}
